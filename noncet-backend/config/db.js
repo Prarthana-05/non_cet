93319759043bce3bbb69186538a541ca3f3f9942
Prarthana-05 <prarthana.ingawale.st@gmail.com>
2025-08-16 13:23:09 -0700

    database change

--- noncet-backend/config/db.js
+++ noncet-backend/config/db.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
+const fs = require('fs'); // <-- this was missing
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
